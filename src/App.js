fb6eed5d792e7817f8c42391598a908300829768
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-11-05 20:08:40 +0200

    User changed to Menu

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
     <div className="container">
       <div className="menu-container">
         <button onClick={onClick} className="menu-trigger">
-          <span>User</span>
+          <span>Menu</span>
           <img
             src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/df/df7789f313571604c0e4fb82154f7ee93d9989c6.jpg"
             alt="User avatar"
